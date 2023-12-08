59766ab8143ceec15331237ae9b5691ac8a87c6d
Carlos Kimura <ckimura@gmail.com>
2023-12-08 17:22:08 -0300

    initial setup of scanner

---
+++ BarcodeScanner/BarcodeScanner/BarcodeScannerView.swift
@@ -0,0 +1,35 @@
+//
+//  BarcodeScannerView.swift
+//  BarcodeScanner
+//
+//  Created by Carlos Kimura on 08/12/23.
+//
+
+import SwiftUI
+
+struct BarcodeScannerView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                Rectangle()
+                    .frame(maxWidth: .infinity, maxHeight: 300)
+                
+                Spacer().frame(height: 60)
+                
+                Label("Scanned Barcode:", systemImage: "barcode.viewfinder")
+                    .font(.title)
+                
+                Text("Not Yet Scanned")
+                    .bold()
+                    .font(.largeTitle)
+                    .foregroundColor(.green)
+                    .padding()
+            }
+            .navigationTitle("cKimi Scanner")
+        }
+    }
+}
+
+#Preview {
+    BarcodeScannerView()
+}
