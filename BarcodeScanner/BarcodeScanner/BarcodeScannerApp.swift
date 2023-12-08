f80e71bddc4e60e2895ac95542da5207ab91d44a
Carlos Kimura <ckimura@gmail.com>
2023-12-08 17:09:50 -0300

    initial commit

---
+++ BarcodeScanner/BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Carlos Kimura on 08/12/23.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
