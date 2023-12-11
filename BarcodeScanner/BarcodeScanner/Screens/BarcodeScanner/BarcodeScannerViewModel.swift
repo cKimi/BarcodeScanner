0b5b730bad451043045b7e0828516e0164bc57f0
Carlos Kimura <ckimura@gmail.com>
2023-12-11 10:32:47 -0300

    refactor, mvvm and project organization

---
+++ BarcodeScanner/BarcodeScanner/Screens/BarcodeScanner/BarcodeScannerViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  BarcodeScannerViewModel.swift
+//  BarcodeScanner
+//
+//  Created by Carlos Kimura on 11/12/23.
+//
+
+import SwiftUI
+
+final class BarcodeScannerViewModel: ObservableObject {
+    
+    @Published var scannedCode = ""
+    @Published var alertItem: AlertItem?
+    
+    var statusText: String {
+        scannedCode.isEmpty ? "Not yet scanned" : scannedCode
+    }
+    
+    var statusTextColor: Color {
+        scannedCode.isEmpty ? .red : .green
+    }
+    
+}
---
+++ BarcodeScanner/BarcodeScanner/Utilities/Alert.swift
@@ -0,0 +1,25 @@
+//
+//  Alert.swift
+//  BarcodeScanner
+//
+//  Created by Carlos Kimura on 11/12/23.
+//
+
+import SwiftUI
+
+struct AlertItem: Identifiable {
+    let id = UUID()
+    let title: String
+    let message: String
+    let dismissButton: Alert.Button
+}
+
+struct AlertContext {
+    static let invalidDeviceInput = AlertItem(title: "Invalid Device Input",
+                                              message: "Something is wrong with the camera. I am unable to capture the input.",
+                                              dismissButton: .default(Text("OK")))
+    
+    static let invalidScannedType = AlertItem(title: "Invalid Device Input",
+                                              message: "The value scanned is not valid. This app scans EAN-8 and EAN-13.",
+                                              dismissButton: .default(Text("OK")))
+}
